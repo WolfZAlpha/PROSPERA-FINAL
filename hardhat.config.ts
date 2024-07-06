f9d7a51ea1876e09f2dcd49a47896e508064bb41
WolfZAlpha <ayowolfz@proton.me>
2024-07-06 14:08:05 -0400

    updates

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,10 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
+import "@typechain/hardhat";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -9,6 +13,16 @@ const config: HardhatUserConfig = {
       optimizer: {
         enabled: true,
       },
+    },
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v6",
+  },
+  networks: {
+    arbitrum: {
+      url: process.env.ARBITRUM_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
 };
