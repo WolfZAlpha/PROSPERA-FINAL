ec010819f94734381bcb53dfa76ff819278182df
WolfZAlpha <137004993+WolfZAlpha@users.noreply.github.com>
2024-07-13 16:46:26 -0400

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,32 +5,33 @@ dotenv.config();
 
 async function main() {
   // Defines the addresses to be used
-  const usdcTokenAddress = "0x22760ae19110018b44337521A41F648e025AcDc4"; // USDC token address on Arbitrum One
-  const gnosisSafeWallet = process.env.GNOSIS_SAFE_ADDRESS as string; // Gnosis Safe multi-sig wallet
-  const taxWallet = "0x233AF064cc70D2d575fD8824B3682948dA3aB94F"; // Tax Wallet Address
-  const stakingWallet = "0xA8d59C804456F1b56b38Fb569641210229Fe051b"; // Staking Rewards Wallet Address
-  const icoWallet = "0xc15C609c4a41D1e247389Cd1B2d8bc4DbDcd4485"; // ICO Wallet Address
-  const liquidityWallet = "0x392f3afA9863baE12CDA1b1E6624E55C8c47A789"; // Liquidity Wallet Address
-  const farmingWallet = "0x03275e133C6cCE9C066247FA7532D0ab86F17926"; // Farming Wallet Address
-  const listingWallet = "0x63824CfF15D7D50537E7A1d47BDc762192b04538"; // Listing Wallet Address
-  const reserveWallet = "0x666B354F231c6Cfd8cA5aD0944559f0420916BB5"; // Reserve Wallet Address
-  const marketingWallet = "0xc0645D8968599A9C8908813174828aca4e9187cE"; // Marketing wallet address
-  const teamWallet = "0x891e4ce655ea6080266b43B7aDc4878af9500353"; // Team wallet address
-  const devWallet = "0x1a95B003d4bE3abb3825f9544912Dd9a6A47aaC4"; // Dev wallet address
-  const prosicoWallet = "0x18DdB071c428ea404Dd899793604EfBfb533Db09"; // prosico supply wallet
+  const gnosisSafeWallet = process.env.GNOSIS_SAFE_ADDRESS as string;
+  const taxWallet = "0x233AF064cc70D2d575fD8824B3682948dA3aB94F";
+  const stakingWallet = "0x93b43c33F4485f8f81fFB2027E51dBa573C393d5";
+  const icoWallet = "0xc15C609c4a41D1e247389Cd1B2d8bc4DbDcd4485";
+  const liquidityWallet = "0x392f3afA9863baE12CDA1b1E6624E55C8c47A789";
+  const farmingWallet = "0x03275e133C6cCE9C066247FA7532D0ab86F17926";
+  const listingWallet = "0x63824CfF15D7D50537E7A1d47BDc762192b04538";
+  const reserveWallet = "0x666B354F231c6Cfd8cA5aD0944559f0420916BB5";
+  const marketingWallet = "0xc0645D8968599A9C8908813174828aca4e9187cE";
+  const teamWallet = "0x891e4ce655ea6080266b43B7aDc4878af9500353";
+  const devWallet = "0x1a95B003d4bE3abb3825f9544912Dd9a6A47aaC4";
+  const prosicoWallet = "0xF1dBbe7e8f5e9e2c463342C17fA35FBEA303762D";
 
   // Defines the vesting wallets and types (0 for marketing, 1 for team)
   const vestingWallets = [
-    { address: "0xb6b6E3a54BCAF861ac456b38D15389dC8E638450", vestingType: 0, amount: ethers.parseEther("1000000") }, // mi1
-    { address: "0x0fcD04410E6DA9339c1578C9f7aC1e48AED4B73C", vestingType: 0, amount: ethers.parseEther("1000000") }, // bns
-    { address: "0x156841B0541F11522656A8FA6d0542B737754E8e", vestingType: 0, amount: ethers.parseEther("1000000") }, // Mch
-    { address: "0x87715D8cC9F32e694CB644fce3b86F4C7311aD15", vestingType: 0, amount: ethers.parseEther("1000000") }, // gt
-    { address: "0x4c6A8Ff3bADe54BCFf3c63Aa84Cb8985c68F0A30", vestingType: 0, amount: ethers.parseEther("1000000") }, // linx
-    { address: "0x3bda56ef07bf6f996f8e3defddde6c8109b7e7be", vestingType: 0, amount: ethers.parseEther("1000000") }, // goul
-    { address: "0x0c45809731a3E88373b63DcA6A1a19dE98843568", vestingType: 0, amount: ethers.parseEther("1000000") }, // bks
-    { address: "0xB50516982524DFF3d8d563F46AD54891Aa61944E", vestingType: 1, amount: ethers.parseEther("2000000") }, // fl
-    { address: "0x89D6a038D902fEAb8c506C3F392b1B91CA8461B7", vestingType: 1, amount: ethers.parseEther("2000000") }, // 7w
-    { address: "0x810999FAAe498DCb4e46736c6f901DDCd51D3a01", vestingType: 1, amount: ethers.parseEther("2000000") }, // z
+    { address: "0xb6b6E3a54BCAF861ac456b38D15389dC8E638450", vestingType: 0, amount: ethers.parseEther("600000") },
+    { address: "0x0fcD04410E6DA9339c1578C9f7aC1e48AED4B73C", vestingType: 0, amount: ethers.parseEther("1000000") },
+    { address: "0x156841B0541F11522656A8FA6d0542B737754E8e", vestingType: 0, amount: ethers.parseEther("60000") },
+    { address: "0x87715D8cC9F32e694CB644fce3b86F4C7311aD15", vestingType: 0, amount: ethers.parseEther("60000") },
+    { address: "0x4c6A8Ff3bADe54BCFf3c63Aa84Cb8985c68F0A30", vestingType: 0, amount: ethers.parseEther("60000") },
+    { address: "0x3bda56ef07bf6f996f8e3defddde6c8109b7e7be", vestingType: 0, amount: ethers.parseEther("60000") },
+    { address: "0xA2526C8DD2560ef4ad8D0A8E2d8201819A92Ae96", vestingType: 0, amount: ethers.parseEther("60000") },
+    { address: "0x0c45809731a3E88373b63DcA6A1a19dE98843568", vestingType: 0, amount: ethers.parseEther("3000000") },
+    { address: "0xC0fF3Af640B344AaDfdC8909BF9826D452bf1718", vestingType: 1, amount: ethers.parseEther("200000") },
+    { address: "0xB50516982524DFF3d8d563F46AD54891Aa61944E", vestingType: 1, amount: ethers.parseEther("35000000") },
+    { address: "0x89D6a038D902fEAb8c506C3F392b1B91CA8461B7", vestingType: 1, amount: ethers.parseEther("40000000") },
+    { address: "0x1a95B003d4bE3abb3825f9544912Dd9a6A47aaC4", vestingType: 1, amount: ethers.parseEther("60000000") },
   ];
 
   console.log("Deploying PROSPERA contracts with the Gnosis Safe as the deployer...");
